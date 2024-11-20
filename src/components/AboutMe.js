e9f0f4c521b32b2f44443ddcbb646da7d99d8e3a
rainof <13416303+rainof@users.noreply.github.com>
2024-11-20 19:32:23 +0700

    feat(frontend): update About Me with additional info and refine styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,78 +4,38 @@ import Image2023 from "../images/ignore/aboutme_grad_doctor_2.jpg";
 import Image2020 from "../images/ignore/aboutme_grad_master_2.jpg";
 import Image2018 from "../images/ignore/aboutme_naripa.jpg";
 import Image2017 from "../images/ignore/aboutme_grad_bachelor.jpg";
+import Aboutme from "../data/aboutme.json";
 
 function AboutMe() {
   const [expandedYear, setExpandedYear] = useState(null);
-
-  const timelineEvents = [
-    {
-      year: "2024",
-      title: "Title",
-      organization: "Affiliation",
-      description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies vitae mollis quisque, eleifend convallis placerat platea.",
-      imageUrl: Image2024,
-    },
-    {
-      year: "2023",
-      title: "Title",
-      organization: "Affiliation",
-      description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies vitae mollis quisque, eleifend convallis placerat platea.",
-      imageUrl: Image2023,
-    },
-    {
-      year: "2020",
-      title: "Title",
-      organization: "Affiliation",
-      description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies vitae mollis quisque, eleifend convallis placerat platea.",
-      imageUrl: Image2020,
-    },
-    {
-      year: "2018",
-      title: "Title",
-      organization: "Affiliation",
-      description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies vitae mollis quisque, eleifend convallis placerat platea.",
-      imageUrl: Image2018,
-    },
-    {
-      year: "2017",
-      title: "Title",
-      organization: "Affiliation",
-      description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies vitae mollis quisque, eleifend convallis placerat platea.",
-      imageUrl: Image2017,
-    },
-  ];
 
   return (
     <div className="h-[90vh] bg-orange-200/60 rounded-[30px]">
       <h1 className="text-3xl font-bold pl-8 pt-8">About Me</h1>
       <div className="container mx-auto px-4 py-8">
         <div className="space-y-8">
-          {timelineEvents.map((item) => (
-            <div key={item.year} className="flex items-start">
-              <div className="w-24 flex-shrink-0 text-xl font-bold">
-                {item.year}
-              </div>
-              <div className="flex-grow">
-                <p className="mb-2">{item.description}</p>
-                <div
-                  className={`relative overflow-hidden transition-all duration-300 ease-in-out ${
-                    expandedYear === item.year ? "max-h-[300px]" : "max-h-0"
-                  }`}
-                  onMouseEnter={() => setExpandedYear(item.year)}
-                  onMouseLeave={() => setExpandedYear(null)}
-                >
-                  <img
-                    src={item.imageUrl}
-                    alt={`Event from ${item.year}`}
-                    className="w-full h-auto"
-                  />
+          {Aboutme.map((item) => (
+            <div key={item.id} className="flex items-start space-x-4">
+              <div className="flex-shrink-0">
+                <div className="w-[100px] h-[100px] flex items-center justify-center bg-white rounded-full text-xl font-bold">
+                  {item.year}
                 </div>
+              </div>
+              <div className="flex-grow bg-white p-4 rounded-xl shadow-lg">
+                <h2 className="text-2xl font-bold mb-2">{item.title}</h2>
+                <p className="text-gray-600 mb-2">{item.description}</p>
+                {item.grad && (
+                  <p className="text-gray-500">
+                    <span className="font-semibold">Graduation:</span>{" "}
+                    {item.grad}
+                  </p>
+                )}
+                {item.organization && (
+                  <p className="text-gray-500">
+                    <span className="font-semibold">Organization:</span>{" "}
+                    {item.organization}
+                  </p>
+                )}
               </div>
             </div>
           ))}
