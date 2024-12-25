6a624d5444f5110e49ae0da992b63374e4ce00c5
rainof <13416303+rainof@users.noreply.github.com>
2024-12-26 02:00:45 +0700

    style(frontend): refine mobile responsiveness styling for About Me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,49 +6,60 @@ function AboutMe() {
 
   return (
     <div className="flex items-center justify-center">
-      {/* <div className="mt-[6vh] w-screen sm:w-[80vw] lg:w-[60vw] h-auto bg-orange-200/60 rounded-[30px]"> */}
       <div className="mt-[6vh] w-screen sm:w-[80vw] xl:w-[60vw] h-auto rounded-[30px]">
         <h1 className="text-3xl font-bold pl-8 pt-8">About Me</h1>
         <div className="container mx-auto px-4 py-8">
           <div className="space-y-8">
             {Aboutme.map((item) => (
-              <div key={item.year} className="flex items-start space-x-4">
+              <div
+                key={item.year}
+                className="flex flex-col sm:flex-row items-start space-y-4 sm:space-y-0 sm:space-x-4"
+              >
                 <div className="flex-shrink-0">
-                  <div className="w-[70px] h-[70px] flex items-center justify-center bg-orange-100 rounded-full text-xl font-bold">
+                  <div className="w-[70px] h-[70px] flex items-center justify-center bg-orange-100 rounded-full text-xl font-bold sm:mb-0 mb-4">
                     {item.year}
                   </div>
                 </div>
-                <div className="flex flex-grow bg-white p-4 rounded-xl shadow-lg">
-                  <div>
-                    <h2 className="text-2xl font-bold mb-2">{item.title}</h2>
-                    {item.school && (
-                      <h2 className="text-2xl font-bold mb-2">{item.school}</h2>
-                    )}
 
-                    {/* Check if description is an array and display it as a list */}
-                    {Array.isArray(item.description) ? (
-                      <ul className="list-disc pl-6 text-gray-600 mb-2">
-                        {item.description.map((desc, index) => (
-                          <li key={index} className="leading-relaxed">
-                            {desc}
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p className="text-gray-600 mb-2">{item.description}</p>
+                <div className="flex flex-grow bg-white p-8 rounded-xl shadow-lg transition-transform hover:scale-[1.02]">
+                  <div>
+                    <h2 className="text-lg sm:text-xl font-bold">
+                      {item.title}
+                    </h2>
+
+                    {(item.school || item.organization) && (
+                      <h2 className="text-md sm:text-lg font-medium mb-4">
+                        {item.school}, {item.organization}
+                      </h2>
                     )}
 
                     {item.grad && (
-                      <p className="text-gray-500">
+                      <p className="text-sm sm:text-base mb-4">
                         <span className="font-semibold">Graduation:</span>{" "}
                         {item.grad}
                       </p>
                     )}
-                    {item.organization && (
-                      <p className="text-gray-500">
-                        <span className="font-semibold">Organization:</span>{" "}
-                        {item.organization}
-                      </p>
+
+                    {Array.isArray(item.description) ? (
+                      <>
+                        <p className="mb-2">
+                          <span className="text-sm sm:text-base font-semibold">
+                            Detail:
+                          </span>
+                        </p>
+                        <ul className="list-disc pl-6 mb-2">
+                          {item.description.map((desc, index) => (
+                            <li
+                              key={index}
+                              className="leading-relaxed text-sm sm:text-base"
+                            >
+                              {desc}
+                            </li>
+                          ))}
+                        </ul>
+                      </>
+                    ) : (
+                      <p className="text-gray-600 mb-2">{item.description}</p>
                     )}
                   </div>
                 </div>
